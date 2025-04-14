cda5a1d851092f39b999f8b607efb0513d149e38
Antônio Evaldo <antonio.evaldo349@gmail.com>
2025-04-14 18:38:04 -0300

    Mão na massa: Aprimorando transições dos cards

--- 01-calmaria-spa/main.js
+++ 01-calmaria-spa/main.js
@@ -8,5 +8,5 @@ const observador = new IntersectionObserver((entradas) => {
   threshold: 0.3,
 });
 
-const elementos = document.querySelectorAll('.animar');
-elementos.forEach((elemento) => observador.observe(elemento));
+  const elementos = document.querySelectorAll('.animar');
+  elementos.forEach((elemento) => observador.observe(elemento));
